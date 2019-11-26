fe35a08ea4f0abb90076732c4c17a2c314ac1fb2
Manuel Aleman <many_98@outlook.es>
2019-11-25 20:38:09 -0600

    Relacion 1:n n:m y accesors

---
+++ app/Disco.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Disco extends Model
+{
+  
+   protected $fillable = ['genero','año','album','formato'];
+   
+   //un disco pertenece a muchos artistas
+   public function famosos(){
+   	  return $this->belongsToMany(Famoso::class);
+   }
+    
+   //un disco pertenece a una disquera
+   public function discografica(){
+   	  return $this->belongsTo(Discografica::class);   
+   	}
+
+   	public function getGeneroAttribute($value){
+   		return ucwords($value);
+   	}
+
+   	public function getAlbumAttribute($value){
+   		return ucwords($value);
+   	}
+
+   	public function getFormatoAttribute($value){
+   		return ucwords($value);
+   	}
+}
